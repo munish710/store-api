b94cd7205cef1256b63ff6cb74a675f8c1836b17
munish710 <munish710@gmail.com>
2021-08-23 00:10:15 +0530

    added feature to filter items based on name, company, featured

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,25 @@
-const getAllProductsStatic = async (req, res, next) => {
-  throw new Error("Hello, testing express-aync-errors");
+const Product = require("../models/product");
 
-  return res.status(200).json({ msg: "product testing route" });
+const getAllProductsStatic = async (req, res, next) => {
+  const products = await Product.find({ featured: true });
+  return res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  return res.status(200).json({ msg: "product route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  return res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
