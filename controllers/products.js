132adf916cb283bb70f80e4a865d165cec3ed712
munish710 <munish710@gmail.com>
2021-08-23 02:03:05 +0530

    added pagination

--- controllers/products.js
+++ controllers/products.js
@@ -32,6 +32,11 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
   return res.status(200).json({ products, nbHits: products.length });
